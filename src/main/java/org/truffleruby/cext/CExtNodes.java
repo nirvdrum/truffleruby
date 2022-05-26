bf2e1ac78389d15d479a50369031db1c3c001886
Benoit Daloze <benoit.daloze@oracle.com>
2022-05-26 19:59:10 +0200

    Migrate SymbolTable to use TruffleString

--- src/main/java/org/truffleruby/cext/CExtNodes.java
+++ src/main/java/org/truffleruby/cext/CExtNodes.java
@@ -1713,7 +1713,7 @@ public class CExtNodes {
         protected Object checkSymbolCStr(Object string,
                 @CachedLibrary(limit = "LIBSTRING_CACHE") RubyStringLibrary strings) {
             final RubySymbol sym = getLanguage().symbolTable.getSymbolIfExists(
-                    strings.getRope(string),
+                    strings.getTString(string),
                     strings.getEncoding(string));
             return sym == null ? nil : sym;
         }
