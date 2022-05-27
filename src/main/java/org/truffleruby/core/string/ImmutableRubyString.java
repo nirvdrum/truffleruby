4ce97656558e55f0914faa8021ad36fe21687bbf
Benoit Daloze <benoit.daloze@oracle.com>
2022-05-27 21:22:40 +0200

    Add RubyStringLibrary#getTEncoding() and replace most usages of RopeNodes.CharacterLengthNode

--- src/main/java/org/truffleruby/core/string/ImmutableRubyString.java
+++ src/main/java/org/truffleruby/core/string/ImmutableRubyString.java
@@ -86,6 +86,11 @@ public class ImmutableRubyString extends ImmutableRubyObject implements TruffleO
     }
 
     @ExportMessage
+    public TruffleString.Encoding getTEncoding() {
+        return encoding.tencoding;
+    }
+
+    @ExportMessage
     protected boolean isRubyString() {
         return true;
     }
